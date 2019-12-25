a526b69f08b5ae2434ce43f91ec6a1357cda74c7
Xavier Xie <xieyuehao@gmail.com>
2019-12-25 17:34:12 +1000

    add multi parameters

--- main.go
+++ main.go
@@ -16,10 +16,24 @@ func main() {
 		fmt.Printf("%s\n", err.Error())
 	}
 
-	answer2, _ := divide(1,0)
+	answer2, _ := divide(1, 0)
 	fmt.Printf("%f\n", answer2)
 
+	total := sum(12.12, 12.34, 45.23, 2.2, 44.56, 67.21)
+	fmt.Printf("%f\n", total)
+
+	numbers := []float64{12.12, 12.34, 45.23, 2.2, 44.56, 67.21}
+	total2 := sum(numbers...)
+	fmt.Print("total2", "%f\n", total2)
 	fmt.Println("Hello")
+}
+
+func sum(values ...float64) float64 {
+	result := 0.0
+	for _, value := range values {
+		result += value
+	}
+	return result
 }
 
 func add(p1, p2 float64) float64 {
